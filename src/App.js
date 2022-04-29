4bd2df18fb8f8441ffc5703e4983642722f7693f
Parvez Islam <parvezislam45@gmail.com>
2022-04-29 11:52:48 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-9xl'>Alhamdulillah</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
