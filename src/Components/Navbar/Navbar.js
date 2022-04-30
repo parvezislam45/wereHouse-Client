6805205e9c98b7da9db6f1b48feb842f6b0b33f2
Parvez Islam <parvezislam45@gmail.com>
2022-04-30 16:38:20 +0600

    added sign up validation and protect route

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {signOut} from 'firebase/auth'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebas.init';
 
 const Navbar = () => {
     const [user] = useAuthState(auth);
+    const handleSignOut =()=>{
+        signOut(auth);
+    }
     return (
         <div>
             <h1>Navbar</h1>
@@ -16,7 +20,13 @@ const Navbar = () => {
                         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">David Bike Mania</span>
                     </a>
                     <div class="flex md:order-2">
-                        <Link to='/login' className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Login</Link>
+                        {
+                            user ?
+                            <button onClick={handleSignOut}>SignOut</button>
+                            :
+                            
+                            <Link to='/login' className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Login</Link>}
+                            {/* <Link to='/login' className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Login</Link> */}
                         <button data-collapse-toggle="mobile-menu-4" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false">
                             <span class="sr-only">Open main menu</span>
                             <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
--- src/firebas.init.js
+++ src/firebas.init.js
@@ -4,14 +4,21 @@ import {getAuth}from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey:process.envREACT_APP_apiKey,
-  authDomain:process.envREACT_APP_authDomain,
-  projectId:process.envREACT_APP_projectId,
-  storageBucket:process.envREACT_APP_storageBucket,
-  messagingSenderId:process.envREACT_APP_messagingSenderId,
-  appId:process.envREACT_APP_appId,
+  // apiKey:process.env.REACT_APP_apiKey,
+  // authDomain:process.env.REACT_APP_authDomain,
+  // projectId:process.env.REACT_APP_projectId,
+  // storageBucket:process.env.REACT_APP_storageBucket,
+  // messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  // appId:process.env.REACT_APP_appId,
+
+  apiKey: "AIzaSyDlMaWG7CH9_6M7BBrH-HEDs-U8Rumh82M",
+  authDomain: "david-bike-mania.firebaseapp.com",
+  projectId: "david-bike-mania",
+  storageBucket: "david-bike-mania.appspot.com",
+  messagingSenderId: "525367104180",
+  appId: "1:525367104180:web:697e0f1b4dc202a667fde1"
 };
 
 // Initialize Firebase
